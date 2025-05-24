dd8359bbfea2ddd2dfd3f3a33a5cc7e4cd16cddf
Gagan Gupta <gagangupta2488@gmail.com>
2025-05-24 12:22:57 +0530

    pushproject

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,17 @@
+export default function SideBar(){
+   return (
+        <div className="sidebar" >
+            <div className="bgOverlay"></div>
+            <div className="sidebarContents">
+             <h2>The Brutal Martain Landscape</h2>
+             <div>
+                <p>Description</p>
+                <p>mn nmfg jkgnjkndfg nkgf jkg fg
+                    nknhkgf 
+                    bnbfbgnfkgjk
+                </p>
+             </div>
+             </div>
+        </div>
+    )
+} 
